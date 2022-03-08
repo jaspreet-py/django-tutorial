ea120e2ab7e1ce42d92ef9d8c9f922ba95fde589
Jaspreet Singh <jaspreet159939@gmail.com>
2022-03-09 00:53:19 +0530

    first migration

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Project apps
+    'polls.apps.PollsConfig',
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps/extensions
     'django_extensions'
 ]
 
