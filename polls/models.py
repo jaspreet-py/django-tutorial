48d7fa118046e59288f895686d7f551a4000fed9
Jaspreet Singh <jaspreet159939@gmail.com>
2022-06-27 03:05:40 +0530

    Add helper fuctions for Question and Choice models

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,25 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
+    def __str__(self) -> str:
+        return self.question_text
+
+    def was_published_recently(self) -> bool:
+        """Returns whether a question was published within the previous 24 hours"""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(verbose_name='date published')
 
 
 class Choice(models.Model):
+    def __str__(self) -> str:
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
