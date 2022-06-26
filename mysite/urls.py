7d58c88a19995255bf9c852b021d4f2c9e350fcc
Jaspreet Singh <jaspreet159939@gmail.com>
2022-06-27 03:04:40 +0530

    Add a redirect view to favicon.ico

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path(route='favicon.ico', view=RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
