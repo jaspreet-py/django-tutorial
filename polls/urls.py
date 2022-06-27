adb95993ca939a7afeb140b9f4eca643e66d411f
Jaspreet Singh <jaspreet159939@gmail.com>
2022-06-28 01:02:02 +0530

    * Namespace polls app URLconf * Add url patterns for question detail, results and vote

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(route="", view=views.index, name="index"),
+    path(route="<int:question_id>/", view=views.detail, name="detail"),
+    path(route="<int:question_id>/results/", view=views.results, name="results"),
+    path(route="<int:question_id>/vote/", view=views.vote, name="vote"),
 ]
